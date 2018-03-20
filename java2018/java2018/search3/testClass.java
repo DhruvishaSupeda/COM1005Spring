52baea2c02afc192b047bd4a329687af089ce2ad
Dhruvisha <dhruvishasupeda@gmail.com>
2018-03-20 03:01:25 +0000

    Initial commit

--- java2018/java2018/search3/testClass.java
+++ java2018/java2018/search3/testClass.java
@@ -1,13 +1,20 @@
 
 
-public class testClass.java {
+public class testClass {
 
     public void testCompareTo() {
       Coords coords1 = new Coords(3,4);
       Coords coords2 = new Coords(3,4);
+      Coords coords3 = new Coords(5,7);
+      RamblersState state = new RamblersState(coords1, 1);
+
       System.out.println("Equal coords test: " + coords1.compareTo(coords2));
+      System.out.println("Not equal coords test: " + coords1.compareTo(coords3));
 
+    }
 
+    public static void main(String[] arg) {
+      testCompareTo();
     }
 
 }
