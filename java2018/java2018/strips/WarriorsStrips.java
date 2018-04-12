65ffb47f198495131d15f6e81aced595d981b499
Dhruvisha <dhruvishasupeda@gmail.com>
2018-04-12 21:26:15 +0100

    Added move and carry operators, somehow runs

--- java2018/java2018/strips/WarriorsStrips.java
+++ java2018/java2018/strips/WarriorsStrips.java
@@ -1,5 +1,37 @@
+import pmatch.*;
+import sheffield.*;
+import java.io.*;
+import java.util.*;
+
 public class WarriorsStrips{
   public static void main(String[] args) {
     EasyWriter scr=new EasyWriter();
-    
+
+    StripsOp move = new StripsOp("move from ?p1 to ?p2",
+                                   "Warrior in ?p2",
+                                   "Warrior in ?p1",
+                                   "Warrior in ?p1");
+    StripsOp carry = new StripsOp("carry ?obj from ?p1 to ?p2",
+                                    "Warrior in ?p2|?obj in ?p2",
+                                    "Warrior in ?P1|?obj in ?p1",
+                                    "Warrior in ?P1|?obj in ?p1|not carrying any object?????"); //HOW TO REPRESENT???
+    //StripsOp ladder = new StripsOp~("")
+
+    ArrayList<StripsOp> warriorOps = new ArrayList<StripsOp>();
+    warriorOps.add(move);
+    warriorOps.add(carry);
+
+    //create instance of Strips1, give it the operators, init state & goal state
+
+    Strips1 str=new Strips1(warriorOps,
+                            new MStringVector("Warrior in W|treasure in P|snake in P"),
+                            new MStringVector("Warrior in P"));
+
+
+    //run Strips
+    boolean res=str.run();
+    scr.println("Result is "+res); //result
+    scr.println("Plan is   "+str.getPlan()); //plan
+
+}
 }
