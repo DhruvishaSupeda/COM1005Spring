67f835b2c696487ef2a55f446bd1dba7ff01393e
DhruvishaSupeda <dhruvishasupeda@gmail.com>
2018-04-13 19:17:40 +0100

    Can get the treasure and come back disregarding the snake

--- java2018/java2018/strips/WarriorsStrips.java
+++ java2018/java2018/strips/WarriorsStrips.java
@@ -13,19 +13,28 @@ public class WarriorsStrips{
                                    "Warrior in ?p1");
     StripsOp carry = new StripsOp("carry ?obj from ?p1 to ?p2",
                                     "Warrior in ?p2|?obj in ?p2",
-                                    "Warrior in ?P1|?obj in ?p1",
-                                    "Warrior in ?P1|?obj in ?p1|not carrying any object?????"); //HOW TO REPRESENT???
-    //StripsOp ladder = new StripsOp~("")
+                                    "Warrior in ?p1|?obj in ?p1",
+                                    "Warrior in ?p1|?obj in ?p1");
+    StripsOp ladder = new StripsOp("pickup treasure in P using ladder",
+                                    "pickup treasure in P",
+                                    "treasure in P",
+                                    "Warrior in P|treasure in P");//Dudt carry object?
+    StripsOp hookRope = new StripsOp("pickup treasure in P using rope",
+                                    "pickup treasure in P",
+                                    "treasure in P",
+                                    "Warrior in R|treasure in P|hook in R|rope in R");//Dudt carry object?
 
     ArrayList<StripsOp> warriorOps = new ArrayList<StripsOp>();
     warriorOps.add(move);
     warriorOps.add(carry);
+    warriorOps.add(ladder);
+    warriorOps.add(hookRope);
 
     //create instance of Strips1, give it the operators, init state & goal state
 
     Strips1 str=new Strips1(warriorOps,
-                            new MStringVector("Warrior in W|treasure in P|snake in P"),
-                            new MStringVector("Warrior in P"));
+                            new MStringVector("Warrior in W|treasure in P"),
+                            new MStringVector("Warrior in R|pickup treasure in P"));//CHANGE GOAL STATE
 
 
     //run Strips
