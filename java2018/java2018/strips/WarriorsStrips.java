983bd781cc25a370014643cfc6777cc5b90abb91
Dhruvisha <dhruvishasupeda@gmail.com>
2018-04-28 14:22:25 +0100

    Swapped carry order so it works

--- java2018/java2018/strips/WarriorsStrips.java
+++ java2018/java2018/strips/WarriorsStrips.java
@@ -8,14 +8,14 @@ public class WarriorsStrips{
     EasyWriter scr=new EasyWriter();
 
 
-    StripsOp ladder = new StripsOp("pickup treasure in P using ladder",
-                                    "pickup treasure in ",
-                                    "treasure in P",
-                                    "ladder in P|treasure in P|snake not in P|Warrior in P");//Dudt carry object?*/
-    StripsOp hookRope = new StripsOp("pickup treasure in P using rope and hook",
-                                    "pickup treasure in P",
-                                    "treasure in P",
-                                    "hook created");//Dudt carry object?
+    StripsOp ladder = new StripsOp("lower ladder into P",
+                                    "treasure available",
+                                    "",
+                                    "snake not in P|ladder in P|Warrior in P");
+    StripsOp hookRope = new StripsOp("lower hook and rope into P",
+                                    "treasure available",
+                                    "",
+                                    "hook created|snake in P|Warrior in P");
     StripsOp move = new StripsOp("move from ?r1 to ?r2",
                                    "Warrior in ?r2",
                                    "Warrior in ?r1",
@@ -23,11 +23,15 @@ public class WarriorsStrips{
     StripsOp carry = new StripsOp("carry ?obj from ?r1 to ?r2",
                                     "Warrior in ?r2|?obj in ?r2",
                                     "Warrior in ?r1|?obj in ?r1",
-                                    "Warrior in ?r1|?obj in ?r1");
+                                    "?obj in ?r1|Warrior in ?r1");
     StripsOp hookCreate = new StripsOp("create hook",
                                     "hook created",
                                     "",
-                                    "hook in R|rope in R|snake in P|Warrior in H");
+                                    "hook in R|rope in R|Warrior in R");
+    StripsOp pickup = new StripsOp("pickup treasure",
+                                    "picked up treasure",
+                                    "",
+                                    "treasure available");
 
 
     ArrayList<StripsOp> warriorOps = new ArrayList<StripsOp>();
@@ -37,12 +41,13 @@ public class WarriorsStrips{
     warriorOps.add(move);
     warriorOps.add(carry);
     warriorOps.add(hookCreate);
+    warriorOps.add(pickup);
 
     //create instance of Strips1, give it the operators, init state & goal state
 
     Strips1 str=new Strips1(warriorOps,
-                            new MStringVector("Warrior in W|treasure in P|hook in H|ladder in L|rope in R|snake in P"),
-                            new MStringVector("pickup treasure in P"));//CHANGE GOAL STATE //Warrior in R|
+                            new MStringVector("Warrior in W|treasure in P|hook in H|rope in R|ladder in L|snake in P"),
+                            new MStringVector("picked up treasure"));//CHANGE GOAL STATE //Warrior in R|
 
 
     //run Strips
